8406e3763b947dc55873360b5bd79b1394a73e50
Joselaine Romão <55799037+joselainejrs@users.noreply.github.com>
2022-10-18 23:19:03 -0300

    Component otimizado para titulo

--- web/src/app/comic-books/comic-books.module.ts
+++ web/src/app/comic-books/comic-books.module.ts
@@ -4,23 +4,27 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { ComicBooksService } from './comic-books.service';
+
 import { CardBooksComponent } from './card-books/card-books.component';
 import { DetailsBooksComponent } from './details-books/details-books.component';
 import { HeaderComponent } from './header/header.component';
 import { HqBooksComponent } from './hq-books/hq-books.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [ 
     CardBooksComponent,
     DetailsBooksComponent,
     HeaderComponent,
-    HqBooksComponent    
+    HqBooksComponent,
+    TitleComponent    
   ],
   exports:[
     CardBooksComponent,
     DetailsBooksComponent,
     HeaderComponent,
-    HqBooksComponent  
+    HqBooksComponent,
+    TitleComponent
   ],
   imports: [
     CommonModule, 
