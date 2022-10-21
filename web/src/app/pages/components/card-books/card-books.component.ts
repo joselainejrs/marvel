e7632fb9f58b2fdbd29fd9ee0e8d65f080c6ae3e
Joselaine Romão <55799037+joselainejrs@users.noreply.github.com>
2022-10-21 00:24:22 -0300

    tratamento da exibição do card e layout

--- web/src/app/pages/components/card-books/card-books.component.ts
+++ web/src/app/pages/components/card-books/card-books.component.ts
@@ -1,29 +1,21 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-card-books',
   templateUrl: './card-books.component.html',
-  styleUrls: [ './card-books.component.scss' ]
+  styleUrls: ['./card-books.component.scss']
 })
 export class CardBooksComponent implements OnInit {
-  // details = false;
-  
-  // @Output() displayDetailsCard = new EventEmitter();
-  // @Output() displayDetailsCard = new EventEmitter();
-  comicBooks = true;
-  
-  @Input() item = false;
+  listBlock = true;
 
-  // @Output() detailsCard: this.comicBooks = false;
-
-  constructor( 
+  constructor(
     private router: Router
   ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
-  displayDetails() {
-    this.router.navigate(['/comic/hqdetails']);
+  toggle() {
+    this.listBlock = false;
+    return this.router.navigate(['/comic/hqdetails']);
   }
 }
