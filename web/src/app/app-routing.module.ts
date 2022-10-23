0f6294a320f21079e0546b703a6289caed87c34c
Joselaine Romão <55799037+joselainejrs@users.noreply.github.com>
2022-10-22 23:54:02 -0300

    retirada de componentes sem uso

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,35 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ComicBooksComponent } from './pages/comic-books/comic-books.component';
+
 import { DetailsBooksComponent } from './pages/comic-books/details-books/details-books.component';
 import { HqBooksComponent } from './pages/comic-books/hq-books/hq-books.component';
-
 import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterAddressComponent } from './pages/register/register-address/register-address.component';
 import { RegisterDiceComponent } from './pages/register/register-dice/register-dice.component';
 import { RegisterPaymentComponent } from './pages/register/register-payment/register-payment.component';
 
-import { RegisterComponent } from './pages/register/register.component';
-
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   
   { path: 'login', component: LoginComponent },
 
-  { path: 'register', component: RegisterComponent },
   { path: 'register/dice', component: RegisterDiceComponent },
   { path: 'register/payment', component: RegisterPaymentComponent },
   { path: 'register/address', component: RegisterAddressComponent },
   
-  { path: 'comic', component: ComicBooksComponent },
   { path: 'comic/hqlist', component: HqBooksComponent },
   { path: 'comic/hqdetails', component: DetailsBooksComponent },
 
   { path: 'profile', component: ProfileComponent },
 
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
