f5375094e85057ea1cc9400db7ee10c64670c1ab
Joselaine Romão <55799037+joselainejrs@users.noreply.github.com>
2023-07-27 01:23:31 -0300

    construção do novo card de pagamento e estrutura css

--- web/src/app/components/card-pay/card-pay.component.ts
+++ web/src/app/components/card-pay/card-pay.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card-pay',
   templateUrl: './card-pay.component.html',
-  styleUrls: ['./card-pay.component.scss']
+  styleUrls: [
+    './card-pay.component.scss',
+    '../../../assets/styles/form.scss'
+  ]
 })
 
 export class CardPayComponent implements OnInit {
--- web/src/app/components/title/title.component.ts
+++ web/src/app/components/title/title.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss']
+  styleUrls: [
+    './title.component.scss',
+    '../../../assets/styles/form.scss'
+  ]
 })
 export class TitleComponent implements OnInit {
 
